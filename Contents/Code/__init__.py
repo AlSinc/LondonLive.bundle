91420cff3b24bf35d8558d24f1fb77aeb2890191
Alasdair Sinclair <alasdairsinc@gmail.com>
2014-05-03 03:57:14 +0100

    Handle live stream being off air better. Add A to Z.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,8 +1,9 @@
 TITLE = 'London Live'
 PREFIX = '/video/londonlive'
 
-URL = 'http://www.londonlive.co.uk'
-URL_LIVE = URL + '/tv'
+LL_URL = 'http://www.londonlive.co.uk'
+LL_URL_LIVE = LL_URL + '/tv'
+LL_URL_A_TO_Z = LL_URL + '/schedule/letter/a'
 
 def Start():
         Log.Debug('Start')
@@ -12,44 +13,83 @@ def Start():
 def Main():
         oc = ObjectContainer(title2 = 'London Live')
         
-	content = HTML.ElementFromURL(URL_LIVE, errors='ignore', cacheTime=1800)
+	content = HTML.ElementFromURL(LL_URL_LIVE, errors='ignore', cacheTime=1800)
 
-        thumb = content.xpath('//video/@poster')[0]
-        
-        oc.add(VideoClipObject(title = 'Live',
-                               summary = 'Live stream',
-                               thumb = thumb,
-                               url = URL_LIVE))
+        thumb = content.xpath('//video/@poster')
+        if len(thumb)>0:
+                thumb = thumb[0]
+        else:
+                thumb = ''
+ 
+                oc.add(VideoClipObject(title = 'Live',
+                                       summary = 'Live stream',
+                                       thumb = thumb,
+                                       url = LL_URL_LIVE))
 
         oc.add(DirectoryObject(title = 'Categories',
                                key = Callback(GetCategories),
-                               summary = 'Browse programms by Category'))
+                               summary = 'Browse programmes by Category'))
+
+        oc.add(DirectoryObject(title = 'A to Z',
+                               key = Callback(GetAtoZ),
+                               summary = 'Browse programmes by A-Z'))
         return oc
 
 def GetCategories():
         oc = ObjectContainer(title2 = 'Categories')
         
-        content = HTML.ElementFromURL(URL, errors='ignore', cacheTime=1800)
+        content = HTML.ElementFromURL(LL_URL, errors='ignore', cacheTime=1800)
         categories = content.xpath("//li[@class='categories__item']/a")
 
         for category in categories:
                 category_path = category.xpath("@href")[0]
                 title = category.text
-                oc.add(DirectoryObject(key=Callback(GetVideosInCategory, url=URL + category_path), title=title))
+                oc.add(DirectoryObject(key=Callback(GetVideosInCategory, url=LL_URL + category_path), title=title))
+        return oc
+
+def GetAtoZ():
+        oc = ObjectContainer(title2 = 'A to Z')
+
+        content = HTML.ElementFromURL(LL_URL_A_TO_Z, errors='ignore', cacheTime=1800)
+        letters = content.xpath("//div[@class='atoz-nav js-atoz-nav']/ul[@class='atoz-nav__list']/li[@class='atoz-nav__item']/a")
+        Log.Debug('letters found: %d'%len(letters))
+        for letter in letters:
+                oc.add(DirectoryObject(title = letter.text,
+                                       key = Callback(GetVideosByLetter, letter = letter.text, url = LL_URL + letter.get('href'))
+                                      ))
+
         return oc
 
 def GetVideosInCategory(url):
         oc = ObjectContainer()
         content = HTML.ElementFromURL(url)
-        programms = content.xpath("//div[@class='clip-tile']")
+        programmes = content.xpath("//div[@class='clip-tile']")
 
-        for program in programms:
-                playable = program.xpath(".//a[@class='clip-tile__play']")
+        for programme in programmes:
+                playable = programme.xpath(".//a[@class='clip-tile__play']")
                 if len(playable) > 0:
-                        title = program.xpath(".//h3[@class='clip-tile__title']")[0].text
-                        thumb = program.xpath("a/span/noscript/img/@src")[0]
-                        video_url = program.xpath("a[@class='clip-tile__link']/@href")[0]
-                        oc.add(VideoClipObject(title=title, thumb=thumb, url=URL+video_url))
+                        title = programme.xpath(".//h3[@class='clip-tile__title']")[0].text
+                        thumb = programme.xpath("a/span/noscript/img/@src")[0]
+                        desc = programme.xpath(".//p[@class='clip-tile__text']")[0].text
+                        video_url = programme.xpath("a[@class='clip-tile__link']/@href")[0]
+                        oc.add(VideoClipObject(title=title, thumb=thumb, 
+                                               url=LL_URL+video_url,
+                                               summary = desc))
                         Log.Debug(title)
                         Log.Debug(thumb)
         return oc
+
+def GetVideosByLetter(letter, url):
+        oc = ObjectContainer(title2 = letter)
+
+        content = HTML.ElementFromURL(url, errors='ignore', cacheTime=1800)
+        programmes = content.xpath("//div[@class='tv-programme__info']/a[@class='tv-programme__link']")
+        for programme in programmes:
+                prog_url = programme.get('href')
+                prog_title = programme.xpath(".//h3[@class='tv-programme__title']")[0].text
+                prog_desc = programme.xpath(".//p[@class='tv-programme__description']")[0].text
+                oc.add(VideoClipObject(title = prog_title, 
+                                       url = LL_URL + prog_url,
+                                       summary = prog_desc))
+        return oc
+
