18fb9734e754be79f399f4b95ba899e706cff6cf
Alasdair Sinclair <alasdairsinc@gmail.com>
2014-05-03 01:36:10 +0100

    Also use the URL Service for Live TV stream. Extract broadcast date meta-data. Filter out programms that can't be played.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -10,53 +10,46 @@ def Start():
 
 @handler(PREFIX, TITLE)
 def Main():
-        oc = ObjectContainer(title2 = 'Live Stream')
+        oc = ObjectContainer(title2 = 'London Live')
         
 	content = HTML.ElementFromURL(URL_LIVE, errors='ignore', cacheTime=1800)
 
-        url = content.xpath('//video/source/@src')[0]
         thumb = content.xpath('//video/@poster')[0]
-        Log.Debug(url)
         
-        oc.add(VideoClipObject(url = url,
-                               title = 'Live', 
+        oc.add(VideoClipObject(title = 'Live',
                                summary = 'Live stream',
                                thumb = thumb,
-                               items = [ MediaObject( parts = [ 
-                                                        PartObject( key = HTTPLiveStreamURL(url)) 
-                                                        ] 
-                                                     )
-                                        ]
-                                )
-                )
+                               url = URL_LIVE))
+
         oc.add(DirectoryObject(title = 'Categories',
                                key = Callback(GetCategories),
                                summary = 'Browse programms by Category'))
         return oc
 
 def GetCategories():
-        oc = ObjectContainer()
+        oc = ObjectContainer(title2 = 'Categories')
         
         content = HTML.ElementFromURL(URL, errors='ignore', cacheTime=1800)
         categories = content.xpath("//li[@class='categories__item']/a")
 
         for category in categories:
-                url = category.xpath("@href")[0]
+                category_path = category.xpath("@href")[0]
                 title = category.text
-                oc.add(DirectoryObject(key=Callback(GetVideosInCategory, url=url), title=title))
+                oc.add(DirectoryObject(key=Callback(GetVideosInCategory, url=URL + category_path), title=title))
         return oc
 
 def GetVideosInCategory(url):
         oc = ObjectContainer()
-        content = HTML.ElementFromURL(URL + url)
+        content = HTML.ElementFromURL(url)
         programms = content.xpath("//div[@class='clip-tile']")
 
         for program in programms:
-                title = program.xpath(".//h3[@class='clip-tile__title']")[0].text
-                thumb = program.xpath("a/span/noscript/img/@src")[0]
-                url = program.xpath("a[@class='clip-tile__link']/@href")[0]
-                oc.add(VideoClipObject(title=title, thumb=thumb, url=URL+url))
-                Log.Debug(title)
-                Log.Debug(thumb)
+                playable = program.xpath(".//a[@class='clip-tile__play']")
+                if len(playable) > 0:
+                        title = program.xpath(".//h3[@class='clip-tile__title']")[0].text
+                        thumb = program.xpath("a/span/noscript/img/@src")[0]
+                        video_url = program.xpath("a[@class='clip-tile__link']/@href")[0]
+                        oc.add(VideoClipObject(title=title, thumb=thumb, url=URL+video_url))
+                        Log.Debug(title)
+                        Log.Debug(thumb)
         return oc
-
